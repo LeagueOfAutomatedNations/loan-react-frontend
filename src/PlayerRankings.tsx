0a0e0ed09f49106e0c02c508cda7fac03611f90a
thmsn <9084377+thmsndk@users.noreply.github.com>
2021-08-07 16:10:54 +0200

    render player rankings as a table

--- src/PlayerRankings.tsx
+++ src/PlayerRankings.tsx
@@ -1,9 +1,10 @@
 import React, { useState, Fragment, useEffect } from "react";
 import feathersClient from "./feathers";
+import { DataGrid, GridSortModel } from "@material-ui/data-grid";
 
 export function PlayerRankings() {
   // const result = ;
-  const [users, setUsers] = useState([] as Array<{ingame_name:string}>);
+  const [users, setUsers] = useState([] as Array<{ ingame_name: string }>);
 
   useEffect(() => {
     (async () => {
@@ -16,12 +17,42 @@ export function PlayerRankings() {
     };
   }, []);
 
+  const [sortModel, setSortModel] = React.useState<GridSortModel>([
+    {
+      field: "combined",
+      sort: "desc"
+    }
+  ]);
+
+  const columns = [
+    // { field: "screeps_id", headerName: "Screeps Id", flex: 1, },
+    { field: "ingame_name", headerName: "Name", flex: 1, }, // TODO: link to screeps profile?
+    { field: "gcl", headerName: "GCL", flex: 1, },
+    { field: "power", headerName: "Power", flex: 1, },
+    {
+      field: "combined",
+      headerName: "Combined",
+      flex: 1,
+      valueGetter: (params: any) => `${params.getValue(params.id, "gcl") + params.getValue(params.id, "power")}`
+    }
+  ];
+
   return (
     <>
-    <h1> Player Rankings</h1>
-      <ul>
-          {users.map(user => <li>{user.ingame_name}</li>)}
-      </ul>
+      <h1> Player Rankings</h1>
+      <div style={{ display: "flex", height: "100%" }}>
+        <div style={{ flexGrow: 1 }}>
+          <DataGrid
+            autoHeight
+            rows={users}
+            columns={columns}
+            pageSize={50}
+            // checkboxSelection
+            disableSelectionOnClick
+            sortModel={sortModel}
+          />
+        </div>
+      </div>
     </>
   );
 }
